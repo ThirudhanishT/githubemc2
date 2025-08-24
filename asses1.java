d4165702b158877416574a171f88d9cb2f3441f9
ThirudhanishT <thirudhanish5@gmail.com>
2025-08-24 19:10:19 +0530

    asses1.java

---
+++ asses1.java
@@ -0,0 +1,37 @@
+import java.lang.System;
+import java.util.*;
+class asses1
+{
+    public static void main(String args[])
+    {
+         System.out.println("--MENU--");
+         System.out.println("1.To check password");
+         System.out.println("2.To heck factorial");
+         System.out.println("3.To addition of two matrics");
+         System.out.println("4.TO substract of two matrics");
+         System.out.println("5.TO exist");
+         while(true)
+         {
+            System.out.println("Enter your opinion");
+            Scanner scan=new Scanner (Sysstem.in);
+            int choice=scan.nextInt();
+            switch(choice)
+            {
+                case 1:System.out.println("Enter the password");
+                       String[] pass=new String[7];
+                       pass=scan.nextLine();
+                       if(pass.length<=7 && pass.matches(".*[A-Z].*") && pass.matches(".*[a-z].*"))
+                       {
+                         System.out.println("THE PASSWORD IS VALID");
+                       }
+                       else
+                       {
+                        System.out.println("THE PASSWORD IS INVALID");
+                       }
+                  
+                case 5:return;
+            }
+         }
+    }
+
+}

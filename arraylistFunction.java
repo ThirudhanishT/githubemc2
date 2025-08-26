8d036ce0aaba8e3f91466d536526c5d64e2f13e9
ThirudhanishT <thirudhanish5@gmail.com>
2025-08-26 19:25:18 +0530

    arraylistFunction.java

---
+++ arraylistFunction.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+import java.util.*;
+class demo
+{
+    public static void main(String args[])
+    {
+        ArrayList<String>list=new ArrayList<>();
+        list.add("apple");
+        list.add("mango");
+        System.out.println(list);
+          }
+}

37e1c921599473a6a98e124f6179f17f85542fb7
ThirudhanishT <thirudhanish5@gmail.com>
2025-08-27 23:01:44 +0530

    sales.java

--- sales.java
+++ sales.java
@@ -14,15 +14,27 @@ class sales
         {
             System.out.println("Enter the 12 months scales for the store "+(i+1));
             int t=0;
-            for(int j=0;i<a[i].length;i++)
+            for(int j=0;j<a[i].length;j++)
             {
-                 int y;
-                 y=scan.nextInt();
-                 scan.nextLine();
-                 t=t+y;
+                 a[i][j]=scan.nextInt();
+                 t=a[i][j]+t;
             }
             total[i]=t;
         }
+        int z=0;
+        int max;
+        max=a[2][0];
+        System.out.println(Arrays.toString(total));
+            for(int j=0;j<a[2].length;j++)
+            {
+                  if(a[2][j]>max)
+                  {
+                     max=a[2][j];
+                     z=j;
+                  }
+            }
+            System.out.println("MAXIMUM SALES FOR THIRD STORE IS IN THE MONTH :"+(z+1));
+        
     }
 }
 

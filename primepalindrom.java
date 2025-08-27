9d2aa64966fdb17ce9c821e02e675315b046dd56
ThirudhanishT <thirudhanish5@gmail.com>
2025-08-27 18:25:39 +0530

    primepalindrom.java

--- primepalindrom.java
+++ primepalindrom.java
@@ -5,20 +5,21 @@ class primepalindrom
     public static void main(String args[])
     {
         int num=2;
-        int num1=num;
-        int rem;
-        int temp=0;
-        int count=0;
         int n=0;
         while(n<20)
         {
-            while(num!=0)
+           int rem;
+           int temp=0;
+           int count=0;
+           int num1=num;
+           int copy =num; 
+            while(copy!=0)
             {
-            rem=num%10;
-            num=num/10;
+            rem=copy%10;
+            copy=copy/10;
             temp=(temp*10)+rem;
             }
-            for(int j=2;j<=temp;j++)
+            for(int j=1;j<=temp;j++)
             {
             if(temp%j==0)
             {
@@ -30,10 +31,7 @@ class primepalindrom
                 System.out.println(temp);
                 n=n+1;
             }
-            else
-            {
             num=num+1;
-            }
         }
         /*if(temp==num1 && count==2)
         {

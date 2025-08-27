f12c5dc84a79e5307ecd0f3b9df09a4a8d4d3a37
ThirudhanishT <thirudhanish5@gmail.com>
2025-08-27 17:59:17 +0530

    primepalindrom.java

--- primepalindrom.java
+++ primepalindrom.java
@@ -4,26 +4,32 @@ class primepalindrom
 {
     public static void main(String args[])
     {
-        Scanner scan=new Scanner(System.in);
-        int num=scan.nextInt();
+        int num=2
         int num1=num;
         int rem;
         int temp=0;
-        while(num!=0)
+        int count=0;
+        int n=0;
+        while(n<20)
         {
+            while(num!=0)
+            {
             rem=num%10;
             num=num/10;
             temp=(temp*10)+rem;
-        }
-        int count=0;
-        while(n<20)
-        { for(int i=1;i<=temp;i++)
-        {
+            }
+            for(int j=2;j<=temp;j++)
+            {
             if(temp%i==0)
             {
                 count=count+1;
+                i=i+1;
             }
-        }
+            else 
+            {
+                num=num+1;
+            }
+            }
         }
         if(temp==num1 && count==2)
         {

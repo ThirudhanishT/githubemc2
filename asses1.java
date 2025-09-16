33ce6d519aaf00daaac3d728d39f0d604cc2809e
ThirudhanishT <thirudhanish5@gmail.com>
2025-09-16 22:32:01 +0530

    asses1.java

--- asses1.java
+++ asses1.java
@@ -20,7 +20,7 @@ class asses1
             {
                 case 1:System.out.println("Enter the password");
                        String pass=scan.nextLine();
-                       if(pass.length()>=7 && pass.matches(".*[A-Z].*") && pass.matches(".*[a-z].*"))
+                       if(pass.length()>=7 && pass.matches("[a-zA-Z0-9]+"))
                        {
                          System.out.println("THE PASSWORD IS VALID");
                        }

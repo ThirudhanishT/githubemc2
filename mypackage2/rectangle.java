d2ba3e3e113b70b05ba81402bdc7fbd16ea86822
ThirudhanishT <thirudhanish5@gmail.com>
2025-09-17 22:40:59 +0530

    rectangle.java

--- mypackage2/rectangle.java
+++ mypackage2/rectangle.java
@@ -5,17 +5,17 @@ public class rectangle
     double breadth;
     double arear;
     double peri;
-    void getr(double length,double breadth)
+    public void getr(double length,double breadth)
     {
         this.length=length;
         this.breadth=breadth;
     }
-    void calA()
+    public void calA()
     {
         arear=length*breadth;
         System.out.println("The area of the rectangle is :"+arear );
     }
-    void calperir()
+    public void calperir()
     {
         peri=2*length*breadth;
         System.out.println("The perimeter of the rectangle is :"+peri);

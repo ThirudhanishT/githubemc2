129a5da716e521ee63ef5caa0f612da647d0ad49
ThirudhanishT <thirudhanish5@gmail.com>
2025-09-17 23:04:49 +0530

    book.java

---
+++ mypackage1/book.java
@@ -0,0 +1,20 @@
+package my package1;
+public class book
+{
+    public void showTitle(String a)
+    {
+         System.out.println("The title of the book is: "+a);
+    }
+    public void showAuthor(String b)
+    {
+        System.out.println("The author of the book: "+b);
+    }
+    public void showPrice(String c)
+    {
+        System.out.println("The price of the book is: "+c);
+    }
+    public void showPublisher(String d)
+    {
+        System.out.println("The publisher name is: "+d);
+    }
+}

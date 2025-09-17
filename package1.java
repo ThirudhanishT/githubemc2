3057e5557b3183fc8bae5f00e17e10c7e8d650f9
ThirudhanishT <thirudhanish5@gmail.com>
2025-09-17 22:14:48 +0530

    package1.java

---
+++ package1.java
@@ -0,0 +1,63 @@
+import java.lang.System;
+package mypackage;
+
+public class circle
+{
+    double radius;
+    double area;
+    double perimeter;
+    void get(double radius)
+    {
+        this.radius=radius;
+    }
+    void calArea()
+    {
+        area=radius*radius;
+        System.out.println("The area of the circle is: "+area);
+    }
+    void calPeri()
+    {
+        perimeter=(2.0)*(3.14)*radius;
+        System.out.println("The perimeter of the circle is: "+perimeter);
+    }
+}
+
+public class rectangle
+{
+    double length;
+    double breadth;
+    double arear;
+    double peri;
+    void getr(double length,double breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+    }
+    void calA()
+    {
+        arear=length*breadth;
+        System.out.println("The area of the rectangle is :"+arear );
+    }
+    void calperir()
+    {
+        peri=2*length*breadth;
+        Ststem.out.println("The perimeter of the rectangle is :"+peri);
+    }
+}
+
+import java.util.Scanner;
+import mypackage.*;
+class package1
+{
+    public static void main(String []args)
+    {
+       circle c=new circle();
+       c.get(5.0);
+       c.calarea();
+       c.calperi();
+       rectangle r=new rectangle();
+       r.getr(59.0,3.0);
+       r.calA();
+       r.peri(); 
+    }
+}

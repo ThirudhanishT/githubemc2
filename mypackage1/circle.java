a35d266e4cd9571c928392ed6f08481fd1f2bdb2
ThirudhanishT <thirudhanish5@gmail.com>
2025-09-17 22:37:54 +0530

    circle.java

--- mypackage1/circle.java
+++ mypackage1/circle.java
@@ -4,16 +4,16 @@ public class circle
     double radius;
     double area;
     double perimeter;
-    void get(double radius)
+    public void get(double radius)
     {
         this.radius=radius;
     }
-    void calArea()
+    public void calArea()
     {
         area=radius*radius;
         System.out.println("The area of the circle is: "+area);
     }
-    void calPeri()
+    public void calPeri()
     {
         perimeter=(2.0)*(3.14)*radius;
         System.out.println("The perimeter of the circle is: "+perimeter);
